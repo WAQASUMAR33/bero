e181ace5175fd33555b29d04495e572546b961d9
Hannankhan00 <hannankhan00@example.com>
2025-12-24 19:20:58 +0500

    Remove arrows from Quality Assurance, Holidays, and Calendar menu items

--- src/app/admin/components/Sidebar.js
+++ src/app/admin/components/Sidebar.js
@@ -244,7 +244,7 @@ export default function Sidebar({ user }) {
           <path fillRule="evenodd" d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
         </svg>
       ),
-      hasArrow: true,
+      hasArrow: false,
     },
     {
       id: 'holidays',
@@ -256,7 +256,7 @@ export default function Sidebar({ user }) {
           <path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd" />
         </svg>
       ),
-      hasArrow: true,
+      hasArrow: false,
     },
     {
       id: 'calendar',
@@ -268,7 +268,7 @@ export default function Sidebar({ user }) {
           <path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd" />
         </svg>
       ),
-      hasArrow: true,
+      hasArrow: false,
     },
     {
       id: 'policy-procedures',
